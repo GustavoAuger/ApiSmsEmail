149433fc437e8794d480dcf55a35fb499c947d6c
GustavoAuger <gu.auger@duocuc.cl>
2024-11-02 09:53:24 -0300

    se añade procfile y main para ejecutar las api por separado en railway

---
+++ main.py
@@ -0,0 +1,13 @@
+from api1.api import app as api1_app
+from api2.api import app as api2_app
+from threading import Thread
+
+def run_api1():
+    api1_app.run(port=5000)
+
+def run_api2():
+    api2_app.run(port=5001)
+
+if __name__ == "__main__":
+    Thread(target=run_api1).start()
+    Thread(target=run_api2).start()
