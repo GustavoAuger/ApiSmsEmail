f4c116a054961de1bd1fe6b601914a8b30021e90
GustavoAuger <gu.auger@duocuc.cl>
2024-11-03 02:54:03 -0300

    arreglando bug en produccion 1,221

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from api2.app import app as api2_app
 from threading import Thread
 
 def run_api1():
-    api1_app.run
+    api1_app.run(port=5000)
 
 def run_api2():
-    api2_app.run
+    api2_app.run(port=5001)
 
 if __name__ == "__main__":
     Thread(target=run_api1).start()
