d811b8fe1db3effe9fa84afe420450e01495b1bd
GustavoAuger <gu.auger@duocuc.cl>
2024-11-05 20:50:32 -0300

    implementando cambios back-front 1.03 fix bugs2

--- app/config.py
+++ app/config.py
@@ -1,2 +1,7 @@
 class Config:
     SECRET_KEY = 'your_secret_key'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'conmiscotusca2@gmail.com'  
+    MAIL_PASSWORD = 'nnbo fxec wqvd dvaa' 
--- app/models2.py
+++ app/models2.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Integer,String,ForeignKey,Column
-from sqlalchemy.orm import relationship,declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -13,24 +13,8 @@ class User(Base):
     fono = Column(String,nullable=False,unique=True)
     password = Column(String,nullable=False,unique=False)
 
-"""def get_users():
-    response = supabase.table("User").select("*").execute()
-    return response.data
-
-def add_user(user: User):
-    response = supabase.table("User").insert(user.__dict__).execute()
-    return response.data"""
-
-"""def update_user(user_id: str, user: User):
-    response = supabase.from_("User").update(user.__dict__).eq("id", user_id).execute()
-    return response.data
-
-def delete_user(user_id: str):
-    response = supabase.from_("User").delete().eq("id", user_id).execute()
-    return response.data"""
-
-class Campaña(Base):
-    __tablename__ = 'Campaña'
+class Campana(Base):
+    __tablename__ = 'Campana'
     
     id = Column(Integer, primary_key=True)
     nombre_campaña = Column(String, nullable=False)
@@ -47,6 +31,28 @@ class Reporte(Base):
     errores = Column(Integer, default=0)
     enviados = Column(Integer, default=0)
     rebotes = Column(Integer, default=0)
-    campaña_id = Column(Integer, ForeignKey('Campaña.id'), nullable=True)
+    campana_id = Column(Integer, ForeignKey('Campana.id'), nullable=True)
+
+class Envio(Base):
+    __tablename__ = 'Envio'
+    
+    id_envio = Column(Integer, primary_key=True)
+    tipo_envio = Column(Boolean,nullable=False)
+    fk_id_campana = Column(Integer, ForeignKey('Campana.id'), nullable=True)
+
+class Envio_destinatario(Base):
+    __tablename__ = 'Envio_destinatario'
+    
+    id = Column(Integer, primary_key=True)
+    fk_envio = Column(Integer, ForeignKey('Envio.id'), nullable=True)
+    fk_destinatario = Column(Integer, ForeignKey('Destinatario.id'), nullable=True)
+
+class Destinatario(Base):
+    __tablename__ = 'Destinatario'
+    
+    id = Column(Integer, primary_key=True)
+    nombre_destinatario = Column(String, primary_key=True)
+    tipo_envio = Column(Boolean,nullable=False)
+    fk_id_campana = Column(Integer, ForeignKey('Campana.id'), nullable=True)
 
 
