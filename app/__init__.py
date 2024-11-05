8b76f9ad1a06fdc2ee250d29a021488a5a7632d8
GustavoAuger <gu.auger@duocuc.cl>
2024-11-05 20:57:23 -0300

    implementando cambios back-front 1.03 fix bugs3

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from app import config
+from flask_mail import Mail
 
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+    
 
     # Load configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    mail.init_app(app)
 
     # Initialize extensions
     db.init_app(app)
