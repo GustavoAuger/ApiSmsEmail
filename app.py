aa1ea3df064d2d1b0deb06d82b784c97854016e5
GustavoAuger <gu.auger@duocuc.cl>
2024-11-10 06:14:09 -0300

    testing

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from app import create_app, db  # Importa create_app y db desde el paquete app
-
-# Crear la instancia de la aplicación Flask
-app = create_app()
-
-# Crear las tablas en la base de datos (si aún no se han creado)
-with app.app_context():
-    db.create_all()  # Esto crea las tablas definidas en tus modelos
-
-# Iniciar la aplicación Flask
+# Mueve las importaciones dentro del bloque condicional
 if __name__ == "__main__":
+    from app import create_app, db  # Importa create_app y db aquí
+
+    # Crear la instancia de la aplicación Flask
+    app = create_app()
+
+    # Crear las tablas en la base de datos (si aún no se han creado)
+    with app.app_context():
+        db.create_all()  # Esto crea las tablas definidas en tus modelos
+
+    # Iniciar la aplicación Flask
     app.run(debug=True, host='0.0.0.0', port=5000)
